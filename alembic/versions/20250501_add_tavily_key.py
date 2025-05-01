4aec8a0c80427de938ed433a3c0709600d4e39c7
macdonc2 <macdonc2@gmail.com>
2025-05-01 21:12:12 +0000

    Add tavily_api_key column migration

---
+++ alembic/versions/20250501_add_tavily_key.py
@@ -0,0 +1,24 @@
+"""Add tavily_api_key to users
+
+Revision ID: 20250501_add_tavily_api_key
+Revises: 20250430_add_openai_key
+Create Date: 2025-05-02 10:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '20250501_add_tavily_api_key'
+down_revision = '20250430_add_openai_key'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.add_column(
+        'users',
+        sa.Column('tavily_api_key', sa.String(), nullable=True),
+    )
+
+def downgrade():
+    op.drop_column('users', 'tavily_api_key')
